acdf59c6e778a75a0bb392069b05f453e726b42a
rjmaida <rmaida85@outlook.com>
2018-08-17 15:52:09 -0300

    Modificaciones en los modulos

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+
+import {BreadcrumbsComponent} from './breadcrumbs/breadcrumbs.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {HeaderComponent} from './header/header.component';
+import {NopagefoundComponent} from './nopagefound/nopagefound.component';
+
+@NgModule({
+  declarations: [
+    NopagefoundComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
+  ],
+  exports: [
+    NopagefoundComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
+  ]
+})
+
+export class SharedModule {}
